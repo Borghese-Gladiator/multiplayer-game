12f64518f63a16eef496d30ced0267e4fbefdfcd
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-14 15:39:25 -0400

    make API request & show isLoading, data, or errors

---
+++ client/src/components/CreateRoom.js
@@ -0,0 +1,24 @@
+// hook from library
+import { useAsync } from "react-async";
+import LoadingDisplay from './LoadingDisplay';
+
+// You can use async/await or any function that returns a Promise
+const loadPlayer = async ({ playerId }, { signal }) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${playerId}`, { signal })
+  if (!res.ok) throw new Error(res.statusText)
+  return res.json()
+}
+
+export default function CreateRoom() {
+  const { data, error, isPending } = useAsync({ promiseFn: loadPlayer, playerId: 2 })
+  if (isPending) return <LoadingDisplay />
+  if (error) return `Something went wrong: ${error.message}`
+  if (data)
+    return (
+      <div>
+        <strong>Player data:</strong>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
+    )
+  return null
+}
