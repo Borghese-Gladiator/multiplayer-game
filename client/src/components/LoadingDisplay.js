f5be47a337f8aa426cb97962990d75c5bcb3f7b9
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-14 15:38:29 -0400

    create loadingDisplay

---
+++ client/src/components/LoadingDisplay.js
@@ -0,0 +1,35 @@
+// Material UI
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Grid
+} from '@material-ui/core';
+// assets
+import LoadingIconImg from '../images/Wedges-3s-200px.gif';
+
+const useStyles = makeStyles((theme) => ({
+  loadingIcon: {
+    width: 500,
+    height: 500
+  }
+}));
+
+function Loading() {
+  const classes = useStyles();
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ minHeight: '100vh' }}
+    >
+      <Grid item>
+        <img src={LoadingIconImg} alt="Loading..." className={classes.loadingIcon} />
+        <p style={{textAlign: 'center'}}>Loading...</p>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Loading
