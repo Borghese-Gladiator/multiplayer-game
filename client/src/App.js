b5fa31cb4cb44e8b9766c07ee2d08761c7ae9f3b
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-14 15:05:57 -0400

    setup routing - home & create room

--- client/src/App.js
+++ client/src/App.js
@@ -1,66 +1,32 @@
-// Material UI
-import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
 import {
-  Button,
-  Paper,
-  Container,
-  TextField
-} from '@material-ui/core';
-// image assets
-import AnimatedLogoImg from './images/logo.gif';
-import DemoImg from './images/demo-kitten.jpg';
-import MyBackgroundImg from './images/background.png';
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Landing from './components/Landing';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    textAlign: 'center',
-    paddingTop: theme.spacing(20),
-    backgroundImage: `url(${MyBackgroundImg})`,
-    minHeight: '100vh'
-  },
-  content: {
-    display: 'flex',
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  logo: {
-    height: 180
-  },
-  cardRoot: {
-    maxWidth: 345,
-    padding: 30,
-    '& > *': {
-      margin: theme.spacing(0.5),
-      width: '100%'
-    },
-  },
-  media: {
-    height: 200,
-  },
-}));
+function CreateRoom() {
+  return <div>CREATE ROOM</div>
+}
 
 function App() {
-  const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <Container className={classes.content}>
-        <img src={AnimatedLogoImg} className={classes.logo} alt="animated logo" />
-        <Paper className={classes.cardRoot}>
-          <TextField id="standard-basic" label="Enter name here" variant="outlined" />
-          <img
-            src={DemoImg}
-            className={classes.media}
-            alt="Demo Kitten"
-          />
-          <Button variant="contained" color="primary">
-            Play!
-          </Button>
-          <Button color="secondary" variant="contained">Create Private Room</Button>
-        </Paper>
-      </Container>
-    </div>
-
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/create">
+            <CreateRoom />
+          </Route>
+          <Route path="/">
+            <Landing />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
