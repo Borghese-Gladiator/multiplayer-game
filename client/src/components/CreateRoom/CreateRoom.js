e0cf1736165a09d52db6c47405467f0f73a1d933
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-14 19:11:43 -0400

    use RoomSettings & PlayersList

--- client/src/components/CreateRoom/CreateRoom.js
+++ client/src/components/CreateRoom/CreateRoom.js
@@ -1,8 +1,15 @@
+// Material UI components
+import { makeStyles } from "@material-ui/core/styles";
+import { Box, Typography, Grid } from '@material-ui/core';
 // hook from library
 import { useAsync } from "react-async";
 // custom components
+import PlayersList from './PlayersList';
 import RoomSettings from './RoomSettings';
 import LoadingDisplay from './LoadingDisplay';
+// assets
+import MyBackgroundImg from '../../images/background.png';
+import AnimatedLogoImg from '../../images/logo.gif';
 
 // You can use async/await or any function that returns a Promise
 const loadPlayer = async ({ playerId }, { signal }) => {
@@ -11,16 +18,50 @@ const loadPlayer = async ({ playerId }, { signal }) => {
   return res.json()
 }
 
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    textAlign: 'center',
+    paddingTop: theme.spacing(5),
+    backgroundImage: `url(${MyBackgroundImg})`,
+    minHeight: '100vh',
+    color: "white"
+  },
+  content: {
+    display: 'flex',
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+}));
+
 export default function CreateRoom() {
+  const classes = useStyles();
   const { data, error, isPending } = useAsync({ promiseFn: loadPlayer, playerId: 2 })
   if (isPending) return <LoadingDisplay />
   if (error) return `Something went wrong: ${error.message}`
   if (data)
     return (
-      <div>
-        <strong>Player data:</strong>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-        <RoomSettings />
+      <div className={classes.root}>
+        <img src={AnimatedLogoImg} className={classes.logo} alt="animated logo" />
+        <Grid container>
+          <Grid item xs={2}>
+          </Grid>
+          <Grid item xs={4}>
+            <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+              <Typography variant="h4" gutterBottom>Settings</Typography>
+              <RoomSettings hostUser={"James"} />
+            </Box>
+          </Grid>
+          <Grid item xs={4}>
+            <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+              <Typography variant="h4" gutterBottom>Players</Typography>
+              <PlayersList />
+            </Box>
+          </Grid>
+          <Grid item xs={2}>
+          </Grid>
+        </Grid>
       </div>
     )
   return null
