f60bb2b3c7badfccb7ce23a75f238f3ff113e3b1
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-14 14:51:54 -0400

    create landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,71 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Material UI
+import { makeStyles, MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import {
+  CssBaseline, Button,
+  Paper,
+  Container, Box, Card, TextField, CardMedia
+} from '@material-ui/core';
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
+// image assets
+import AnimatedLogoImg from './images/logo.gif';
+import DemoImg from './images/demo-kitten.jpg';
+import MyBackgroundImg from './images/background.png';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    textAlign: 'center',
+    paddingTop: theme.spacing(20),
+    backgroundImage: `url(${MyBackgroundImg})`,
+    minHeight: '100vh'
+  },
+  content: {
+    display: 'flex',
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  logo: {
+    height: 180
+  },
+  cardRoot: {
+    maxWidth: 345,
+    padding: 30,
+    '& > *': {
+      margin: theme.spacing(0.5),
+      width: '100%'
+    },
+  },
+  media: {
+    height: 200,
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Container className={classes.content}>
+        <img src={AnimatedLogoImg} className={classes.logo} alt="animated logo" />
+        <Paper className={classes.cardRoot}>
+          <TextField id="standard-basic" label="Enter name here" variant="outlined" />
+          <img
+            src={DemoImg}
+            className={classes.media}
+            alt="Demo Kitten"
+          />
+          <Button variant="contained" color="primary">
+            Play!
+          </Button>
+          <Button color="secondary" variant="contained">Create Private Room</Button>
+        </Paper>
+      </Container>
     </div>
+
   );
 }
 
