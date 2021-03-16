7b6d0c4214d9078cb32f9de7d2b64e2e4f9c7c5d
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-16 19:23:46 -0400

    add game mode selector

--- client/src/components/CreateRoom/RoomSettings/RoomSettings.js
+++ client/src/components/CreateRoom/RoomSettings/RoomSettings.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // routing
 import { Link } from 'react-router-dom';
 // Material UI Components
@@ -6,6 +6,44 @@ import { makeStyles } from '@material-ui/core/styles';
 import {
   Paper, Box, Typography, Button, Divider
 } from '@material-ui/core';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+
+function PickSettings(props) {
+  const { gameModes } = props;
+  const arrLength = gameModes.length;
+  const [gameModeIndex, setGameModeIndex] = useState(0);
+  const handleNextClick = () => {
+    if (gameModeIndex === arrLength - 1) {
+      setGameModeIndex(0);
+    } else {
+      setGameModeIndex(gameModeIndex + 1); 
+    }
+  }
+  const handlePrevClick = () => {
+    if (gameModeIndex === 0) {
+      setGameModeIndex(arrLength - 1);
+    } else {
+      setGameModeIndex(gameModeIndex - 1); 
+    }
+  }
+
+  return (
+    <div style={{ width: '100%' }}>
+      <Box display="flex" p={1} bgcolor="background.paper">
+        <Button variant="outlined" onClick={handleNextClick}>
+          <ArrowBackIosIcon />
+        </Button>
+        <Box p={1} flexGrow={1} bgcolor="grey.300">
+          { gameModes[gameModeIndex].name }
+        </Box>
+        <Button variant="outlined" onClick={handlePrevClick}>
+          <ArrowForwardIosIcon />
+        </Button>
+      </Box>
+    </div>
+  )
+}
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
@@ -17,13 +55,13 @@ const useStyles = makeStyles((theme) => ({
   },
   root: {
     padding: 5,
-    minWidth: theme.spacing(50)
+    width: '100%'
   },
 }));
 
 function RoomSettings(props) {
   const classes = useStyles();
-  const { roomURL, hostUser } = props;
+  const { gameModes, roomURL, hostUser } = props;
 
   return (
     <Paper className={classes.root}>
@@ -31,6 +69,7 @@ function RoomSettings(props) {
         <Typography variant="h5">Lobby</Typography>
         <Typography variant="body2" gutterBottom>Created by: {hostUser} </Typography>
         <Divider style={{ width: '100%' }} />
+        <PickSettings gameModes={gameModes} />
         <Button component={Link} to={roomURL} color="primary" variant="contained">Start Game</Button>
       </Box>
     </Paper>
