a40203e4342ead80d9924fd051fe52a8ff194903
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-17 18:47:54 -0400

    fix centering problems with Grid by using Box

--- client/src/components/LoadingDisplay/LoadingDisplay.js
+++ client/src/components/LoadingDisplay/LoadingDisplay.js
@@ -2,34 +2,26 @@
 import { makeStyles } from "@material-ui/core/styles";
 import {
   Grid,
-  Typography
+  Typography,
+  Box
 } from '@material-ui/core';
 // assets
 import LoadingIconImg from '../../images/Spinner-1s-200px.gif';
 
 const useStyles = makeStyles((theme) => ({
   loadingIcon: {
-    width: 500,
-    height: 500
+    width: 300,
+    height: 300
   }
 }));
 
 function Loading() {
   const classes = useStyles();
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justify="center"
-      style={{ minHeight: '100vh' }}
-    >
-      <Grid item>
-        <img src={LoadingIconImg} alt="Loading..." className={classes.loadingIcon} />
-        <Typography variant="h4" style={{color: "white", textAlign: 'center'}}>Connecting to Server...</Typography>
-      </Grid>
-    </Grid>
+    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+      <img src={LoadingIconImg} alt="Loading..." className={classes.loadingIcon} />
+      <Typography variant="h4" style={{ color: "white", textAlign: 'center' }}>Connecting to Server...</Typography>
+    </Box>
   )
 }
 
