15edbee261af4290bded966eb51727c82c265bb8
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-17 21:15:11 -0400

    initialize GameChat (no real content)

---
+++ client/src/components/Game/GameChat/GameChat.js
@@ -0,0 +1,12 @@
+// Material UI Components
+import {
+  Box, Grid, Paper, Button, Typography, Container
+} from '@material-ui/core';
+
+export default function GameChat() {
+  return (
+    <Paper>
+      <Typography variant="body1">GAME CHAT WINDOW </Typography>
+    </Paper>
+  )
+}
