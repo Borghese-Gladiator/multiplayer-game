1a0555023030d22f9997b45772d16ebf20db3c36
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-17 21:15:21 -0400

    use new components in 2 column layout

--- client/src/components/Game/Game.js
+++ client/src/components/Game/Game.js
@@ -6,12 +6,15 @@ import socketIOClient from "socket.io-client";
 // Generate usernames
 import userGen from "username-generator"
 // Material UI Components
+import { makeStyles } from "@material-ui/core/styles";
 import {
   Box, Grid, Paper, Button, Typography, Container
 } from '@material-ui/core';
 // custom components
-import CountdownTimer from './CountdownTimer';
 import LoadingDisplay from '../LoadingDisplay';
+import GamePlayersList from './GamePlayersList';
+import PhaseDisplay from './PhaseDisplay';
+import GameChat from './GameChat';
 
 // client-side
 const ENDPOINT = process.env.REACT_APP_NODE_ENV === "production" ? process.env.REACT_APP_API_URL : "http://127.0.0.1:4001";
@@ -22,12 +25,28 @@ const socket = socketIOClient(ENDPOINT, {
   }
 });
 
+function UserWordDisplay({ word }) {
+  return (
+    <Paper>
+      <Typography variant="h6">Your word</Typography>
+      <Typography variant="h3">{word}</Typography>
+    </Paper>
+  )
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    textAlign: 'center',
+  }
+}))
+
 function Game() {
+  const classes = useStyles();
   // We can use the `useParams` hook here to access
   // the dynamic pieces of the URL.
   let { roomID } = useParams();
 
-  const [currentPhase, setCurrentPhase] = useState("VOTING PHASE");
+  const [word, setWord] = useState("MIT");
 
   const [user, setUser] = useState({
     usersList: null
@@ -67,60 +86,24 @@ function Game() {
     socket.emit("sendMsg", JSON.stringify({ id: loggedUser.id, msg: msg }));
   }
 
-  if (!socket.connected) {
+  if (socket.connected) {
     return <LoadingDisplay />
   } else {
     return (
-      <Container>
+      <Container className={classes.root}>
         <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
           <Button variant="contained" color="primary" onClick={() => alert("PAUSED")}>PAUSE TIMER</Button>
-          <h3>Room ID: {roomID}</h3>
-          <Grid container spacing={3}>
+          <Typography variant="h5">You are: {word}</Typography>
+          <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
-              {/* List of Connected Users */}
-              <h3 className="d-flex justify-content-center"> Connected users : {user.usersList?.length} </h3>
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th> User name </th>
-                    <th> Connection Date </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {user.usersList?.map(user => {
-                    return (<tr key={user.id}>
-                      <td> {user.userName} </td>
-                      <td> {user.connectionTime} </td>
-                    </tr>)
-                  })}
-                </tbody>
-              </table>
+              <GamePlayersList />
+              <br />
+              <UserWordDisplay word={word} />
             </Grid>
             <Grid item xs={12} sm={6}>
-              <Paper>
-                <Typography variant="h3">{currentPhase}</Typography>
-                <CountdownTimer />
-              </Paper>
-            </Grid>
-            <Grid item xs={12} sm={6} style={{ minHeight: "50vh" }}>
-              {/*CHATBOX COMPONENT */}
-              <div>
-                <h2 className="d-flex justify-content-center"> Chat </h2>
-                {recMsg.listMsg?.map((msgInfo, index) => {
-                  return (
-                    <div className="d-flex justify-content-center" key={index}>
-                      <b>{msgInfo.userName} </b> :  {msgInfo.msg} <small style={{ marginLeft: "18px", color: "blue", marginTop: "5px" }}> {msgInfo.time} </small>
-                    </div>
-                  )
-                })}
-              </div>
-              <input style={{ width: "300px", display: "inline" }} id="inputmsg" onChange={(event) => setMsg(event.target.value)} />
-              <Button className="btn btn-info" id="btnmsg" onClick={() => { sendMessage() }}> Send </Button>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <h3> name: {loggedUser?.userName} </h3>
-              <Typography variant="h5">YOUR WORD:</Typography>
-              <Typography variant="h3">MIT</Typography>
+              <PhaseDisplay />
+              <br />
+              <GameChat />
             </Grid>
           </Grid>
         </Box>
