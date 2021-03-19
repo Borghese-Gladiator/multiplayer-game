eba39f36def8c109a18ee8cce758240b3d21559a
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-18 20:09:46 -0400

    add actual settings (describe, discussion & voting time dropdowns, & customWords textarea)

--- client/src/components/CreateRoom/RoomSettings/RoomSettings.js
+++ client/src/components/CreateRoom/RoomSettings/RoomSettings.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 // Material UI Components
 import { makeStyles } from '@material-ui/core/styles';
 import {
-  Paper, Box, Typography, Button, Divider
+  Paper, Box, Typography, Button, Divider, TextField, Select, MenuItem, FormControl, FormHelperText, InputLabel
 } from '@material-ui/core';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
@@ -17,14 +17,14 @@ function PickSettings(props) {
     if (gameModeIndex === arrLength - 1) {
       setGameModeIndex(0);
     } else {
-      setGameModeIndex(gameModeIndex + 1); 
+      setGameModeIndex(gameModeIndex + 1);
     }
   }
   const handlePrevClick = () => {
     if (gameModeIndex === 0) {
       setGameModeIndex(arrLength - 1);
     } else {
-      setGameModeIndex(gameModeIndex - 1); 
+      setGameModeIndex(gameModeIndex - 1);
     }
   }
 
@@ -35,7 +35,7 @@ function PickSettings(props) {
           <ArrowBackIosIcon />
         </Button>
         <Box p={1} flexGrow={1} bgcolor="grey.300">
-          { gameModes[gameModeIndex].name }
+          {gameModes[gameModeIndex].name}
         </Box>
         <Button variant="outlined" onClick={handlePrevClick}>
           <ArrowForwardIosIcon />
@@ -48,29 +48,116 @@ function PickSettings(props) {
 const useStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(1),
-    width: '100%'
+    width: '100%',
   },
   selectEmpty: {
-    marginTop: theme.spacing(2),
+    border: '1px solid #ccc',
+    width: '100%',
   },
   root: {
-    padding: 5,
-    width: '100%'
+    padding: theme.spacing(1),
+    width: '100%',
+    minHeight: '300px'
   },
+  content: {
+    '& > *': {
+      margin: theme.spacing(0.5),
+      width: '100%'
+    },
+  }
 }));
 
 function RoomSettings(props) {
   const classes = useStyles();
   const { gameModes, roomURL, hostUser } = props;
+  const [customWordsText, setCustomWordsText] = useState('');
+  const [age, setAge] = React.useState(30);
+  const [roundObj, setRoundObj] = useState({
+    describeTime: 30,
+    discussionTime: 45,
+    voteTime: 45,
+    customWords: []
+  });
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+  const handleTextFieldChange = (event) => {
+    setCustomWordsText(event.target.value);
+  };
 
   return (
     <Paper className={classes.root}>
-      <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" style={{minHeight: '300px'}}>
-        <Typography variant="h5">Lobby</Typography>
-        <Typography variant="body2" gutterBottom>Created by: {hostUser} </Typography>
-        <Divider style={{ width: '100%' }} />
+      <Box display="flex" flexDirection="column" alignItems="flex-start">
+        <Box alignSelf="center">
+          <Typography variant="h5">Lobby</Typography>
+        </Box>
+        <Divider style={{ width: '100%', borderTop: '3px double #8c8b8b' }} />
+
+        <FormControl className={classes.formControl}>
+          <Typography variant="body2">Describe time in seconds</Typography>
+          <Select
+            value={age}
+            onChange={handleChange}
+            displayEmpty
+            className={classes.selectEmpty}
+            inputProps={{ 'aria-label': 'Without label' }}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>10</MenuItem>
+            <MenuItem value={20}>20</MenuItem>
+            <MenuItem value={30}>30</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl className={classes.formControl}>
+          <Typography variant="body2">Discussion time in seconds</Typography>
+          <Select
+            value={age}
+            onChange={handleChange}
+            displayEmpty
+            className={classes.selectEmpty}
+            inputProps={{ 'aria-label': 'Without label' }}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>10</MenuItem>
+            <MenuItem value={20}>20</MenuItem>
+            <MenuItem value={30}>30</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <Typography variant="body2">Voting time in seconds</Typography>
+          <Select
+            value={age}
+            onChange={handleChange}
+            displayEmpty
+            className={classes.selectEmpty}
+            inputProps={{ 'aria-label': 'Without label' }}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>10</MenuItem>
+            <MenuItem value={20}>20</MenuItem>
+            <MenuItem value={30}>30</MenuItem>
+          </Select>
+        </FormControl>
+
+        <Typography variant="body2">Custom Words</Typography>
+        <textarea
+          value={customWordsText}
+          onChange={handleTextFieldChange}
+          placeholder={`Type custom word pairs separated by a comma and then a new line.\nFirst_Word,Second_Word\nMIT,Harvard`}
+          style={{ border: '1px solid #ccc', width: "100%", minHeight: '40px' }}
+        />
         <PickSettings gameModes={gameModes} />
-        <Button component={Link} to={roomURL} color="primary" variant="contained">Start Game</Button>
+        <Box alignSelf="center">
+          <Button component={Link} to={roomURL} color="primary" variant="contained">Start Game</Button>
+        </Box>
       </Box>
     </Paper>
   )
