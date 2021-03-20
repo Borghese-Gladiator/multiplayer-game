36cabbdbbd56e9fb60aca41e827013579deea4b4
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-20 03:16:01 -0400

    move username info & word to info button

---
+++ client/src/components/Game/InfoDialog/InfoDialog.js
@@ -0,0 +1,51 @@
+import React from 'react';
+// Material UI Components
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import Typography from '@material-ui/core/Typography';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+// Material UI Icons
+import InfoIcon from '@material-ui/icons/Info';
+
+function SimpleDialog({ handleClose, open, username, word }) {
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+            <Typography variant="h5">You are: {username}</Typography>
+
+            <Typography variant="h6">Your word</Typography>
+            <Typography variant="h3">{word}</Typography>
+          </Box>
+
+        </DialogContentText>
+      </DialogContent>
+
+    </Dialog>
+  );
+}
+
+export default function SimpleDialogDemo({ username, word }) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (value) => {
+    setOpen(false);
+  };
+
+  return (
+    <Box m={1}>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        <InfoIcon style={{fontSize: 30}} />
+      </Button>
+
+      <SimpleDialog username={username} word={word} open={open} handleClose={handleClose} />
+    </Box>
+  );
+}
