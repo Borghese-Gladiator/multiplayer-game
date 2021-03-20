7a68df475693fc23d436f7036606482305da6fb7
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-20 03:16:24 -0400

    REDO GAME LAYOUT & put InfoDialog above GameChat

--- client/src/components/Game/Game.js
+++ client/src/components/Game/Game.js
@@ -9,13 +9,14 @@ import userGen from "username-generator"
 // Material UI Components
 import { makeStyles } from "@material-ui/core/styles";
 import {
-  Box, Grid, Paper, Button, Typography, Container
+  Box, Grid, Paper, Typography, Container
 } from '@material-ui/core';
 // custom components
 import LoadingDisplay from '../LoadingDisplay';
 import GamePlayersList from './GamePlayersList';
 import PhaseDisplay from './PhaseDisplay';
 import GameChat from './GameChat';
+import InfoDialog from './InfoDialog';
 
 // client-side
 const ENDPOINT = process.env.REACT_APP_NODE_ENV === "production" ? process.env.REACT_APP_API_URL : "http://127.0.0.1:4001";
@@ -25,15 +26,6 @@ const socket = socketIOClient(ENDPOINT, {
     "my-custom-header": "abcd"
   }
 });
-
-function UserWordDisplay({ word }) {
-  return (
-    <Paper>
-      <Typography variant="h6">Your word</Typography>
-      <Typography variant="h3">{word}</Typography>
-    </Paper>
-  )
-}
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -103,18 +95,21 @@ function Game() {
     return (
       <Container className={classes.root}>
         <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-          <Button variant="contained" color="primary" onClick={() => alert("PAUSED")}>PAUSE TIMER</Button>
-          <Typography variant="h5">You are: {loggedUser?.userName}</Typography>
+          <PhaseDisplay />
+          <br />
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={2}>
               <GamePlayersList userList={userList} />
-              <br />
-              <UserWordDisplay word={word} />
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <PhaseDisplay />
-              <br />
-              <GameChat loggedUserID={loggedUser.id} msgList={msgList} sendMessage={sendMessage} />
+            <Grid item xs={10}>
+              <Paper>
+                <Box display="flex" justifyContent="flex-end">
+                  <InfoDialog username={loggedUser?.userName} word={word} />
+                </Box>
+                <Box p={3}>
+                  <GameChat loggedUserID={loggedUser.id} msgList={msgList} sendMessage={sendMessage} />
+                </Box>
+              </Paper>
             </Grid>
           </Grid>
         </Box>
