37de042d196f819e09142991dc769beb3eca4a71
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-20 17:29:32 -0400

    remove container & add box padding

--- client/src/components/Game/GamePlayersList/GamePlayersList.js
+++ client/src/components/Game/GamePlayersList/GamePlayersList.js
@@ -1,29 +1,25 @@
 // Material UI components
-import { Container, Paper, Box, Grid, Typography } from '@material-ui/core';
+import { Paper, Box, Grid, Typography } from '@material-ui/core';
 // Material UI icons
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
 
 function GamePlayersList({ userList }) {
   return (
     <Paper>
-      <Container>
-        <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-          <Typography variant="h4">Players</Typography>
-          <br />
-          <Grid container spacing={3}>
-            {userList.map((val, idx) => {
-              const { id, userName, connectionTime } = val;
-              console.log(`Connection Time: ${connectionTime}`)
-              return (
-                <Grid item xs={12} key={`${id} ${idx}`}>
-                  <AccountBoxIcon style={{ fontSize: "40px" }} />
-                  <Typography variant="h6" style={{ wordWrap: 'break-word' }}>{userName}</Typography>
-                </Grid>
-              )
-            })}
-          </Grid>
-        </Box>
-      </Container>
+      <Box p={1} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+        <Grid container spacing={3}>
+          {userList.map((val, idx) => {
+            const { id, userName, connectionTime } = val;
+            console.log(`Connection Time: ${connectionTime}`)
+            return (
+              <Grid item xs={12} key={`${id} ${idx}`}>
+                <AccountBoxIcon style={{ fontSize: "40px" }} />
+                <Typography variant="h6" style={{ wordWrap: 'break-word' }}>{userName}</Typography>
+              </Grid>
+            )
+          })}
+        </Grid>
+      </Box>
     </Paper>
 
   )
