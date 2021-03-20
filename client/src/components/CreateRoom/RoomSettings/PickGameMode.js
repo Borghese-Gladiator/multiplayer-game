0f48657c32b6260cb1240e739b905968ffe7e59c
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-20 16:55:35 -0400

    make PickGameMode responsive

--- client/src/components/CreateRoom/RoomSettings/PickGameMode.js
+++ client/src/components/CreateRoom/RoomSettings/PickGameMode.js
@@ -1,14 +1,41 @@
 import React, { useState } from 'react'
 // Material UI Components
+import { makeStyles } from '@material-ui/core/styles';
 import {
-  TextField, Paper, Box, Typography, Button, Divider, Select, MenuItem, FormControl
+  Box, Button, FormControl, Select, MenuItem, Typography
 } from '@material-ui/core';
 // Material UI Icons
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
-export default function PickSettings(props) {
-  const { gameModes } = props;
+const useStyles = makeStyles((theme) => ({
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'block',
+    },
+  },
+  formControl: {
+    width: '100%',
+  },
+  formControlLabel: {
+    textAlign: 'left'
+  },
+  selectEmpty: {
+    border: '1px solid #ccc',
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+
+}));
+
+export default function PickSettings({ gameModes }) {
+  const classes = useStyles();
+
   const arrLength = gameModes.length;
   const [gameModeIndex, setGameModeIndex] = useState(0);
   const handleNextClick = () => {
@@ -25,20 +52,42 @@ export default function PickSettings(props) {
       setGameModeIndex(gameModeIndex - 1);
     }
   }
+  
+  const handleIndexChange = (event) => {
+    setGameModeIndex(event.target.value);
+  };
 
   return (
-    <div style={{ width: '100%' }}>
-      <Box display="flex" p={1} bgcolor="background.paper">
-        <Button variant="outlined" onClick={handleNextClick}>
-          <ArrowBackIosIcon />
-        </Button>
-        <Box p={1} flexGrow={1} bgcolor="grey.300">
-          {gameModes[gameModeIndex].name}
+    <div>
+
+      <div className={classes.sectionDesktop} style={{ width: '100%' }}>
+        <Box display="flex" p={1} bgcolor="background.paper">
+          <Button variant="outlined" onClick={handleNextClick}>
+            <ArrowBackIosIcon />
+          </Button>
+          <Box p={1} flexGrow={1} bgcolor="grey.300">
+            {gameModes[gameModeIndex].name}
+          </Box>
+          <Button variant="outlined" onClick={handlePrevClick}>
+            <ArrowForwardIosIcon />
+          </Button>
         </Box>
-        <Button variant="outlined" onClick={handlePrevClick}>
-          <ArrowForwardIosIcon />
-        </Button>
-      </Box>
+      </div>
+      <div className={classes.sectionMobile}>
+        <FormControl className={classes.formControl}>
+          <Typography className={classes.formControlLabel} variant="body2">Game Mode</Typography>
+          <Select
+            value={gameModeIndex}
+            onChange={handleIndexChange}
+          >
+            {gameModes.map((val, idx) => {
+              return (
+                <MenuItem key={`${idx} ${val.name}`} value={idx}>{val.name}</MenuItem>
+              )
+            })}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   )
 }
